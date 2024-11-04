9f0cb1a3f76dc5d095145bfc317b28666e62b6bd
carloslavig <carloslavig@gmail.com>
2024-11-04 18:33:01 -0300

    erro do insidewhile

--- calculator.java
+++ calculator.java
@@ -10,47 +10,72 @@ public class calculator {
         //Declarando a variavel do resultado da calculadora
         float result = 0;
         //Aqui farei a pergunta a respeito dos números envolvidos e qual operação usar
+        float a=0,b=0;
 
-
-        boolean continua;
+        boolean continua= true;
 
         do {
 
+            while (continua) {
+                System.out.print("Primeiro número: ");
+                if (ler.hasNextFloat()) {
+                    a = ler.nextFloat();
+                    ler.nextLine();
+                    continua = false;
+                }else{
+                    ler.nextLine();
+                    System.out.println("Erro: Isso não é um número");
+                }
 
-            System.out.print("Primeiro numero: ");
-            float a = ler.nextFloat();
-            //ler.nextLine();
+            }
+
 
             System.out.print("Informe a operação: ");
             String oper = ler.next();
 
-            System.out.print("Segundo numero: ");
-            float b = ler.nextFloat();
-            //ler.nextLine();
+
+            do {
+                System.out.print("Segundo número: ");
+                if (ler.hasNextFloat()) {
+                    b = ler.nextFloat();
+                    ler.nextLine();
+                    continua = false;
+                }else{
+                ler.nextLine();
+                System.out.println("Erro: Isso não é um número");
+                continua = true;
+                }
+
+            }while (continua);
+
             //Agora é a parte do switch que faz  a calculadora funcionar
 
 
             switch (oper) {
                 case "+":
                     result = a + b;
+                    System.out.println("O resultado da operação é " + result);
                     break;
                 case "-":
                     result = a - b;
+                    System.out.println("O resultado da operação é " + result);
                     break;
                 case "*":
                     result = a * b;
+                    System.out.println("O resultado da operação é " + result);
                     break;
                 case "/":
                     result = a / b;
+                    System.out.println("O resultado da operação é " + result);
                     break;
                 default:
-                    System.out.println("Operação invalida");
+                    System.out.println("Operação invalida, tente novamente");
 
             }
 
-            System.out.println("O resultado da operação é " + result);
 
-            System.out.print("deseja continuar? (S/N): ");
+
+            System.out.print("Deseja realizar uma nova operação? (S/N): ");
             String resposta = ler.next().toUpperCase();
 
             if (resposta.equals("S")) {
